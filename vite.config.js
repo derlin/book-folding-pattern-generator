b97a52fa13243566784c3e96a09498379999369d
Lucy Linder <lucy.derlin@gmail.com>
2025-11-19 07:59:47 +0100

    ci: set the base url to custom domain

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 
+const BASE_URL = process.env.APP_BASE_URL || "";
+
 export default defineConfig({
   // Setting base to an empty string forces relative paths for all assets.
   // Example: /assets/index.js becomes assets/index.js
   // Important for it to work on github actions
-  base: "",
+  base: BASE_URL,
 });
