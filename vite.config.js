a634267035cea7ebb912f6d9e977235e3d8f7707
Lucy Linder <lucy.derlin@gmail.com>
2025-12-05 07:12:26 +0100

    feat: add sitemap and robots.txt

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from "vite";
+import sitemap from "vite-plugin-sitemap";
 
 const BASE_URL = process.env.APP_BASE_URL || "";
+const HOSTNAME = BASE_URL || "http://localhost:5173";
 
 export default defineConfig({
   // Setting base to an empty string forces relative paths for all assets.
   // Example: /assets/index.js becomes assets/index.js
   // Important for it to work on github actions
   base: BASE_URL,
+
+  plugins: [
+    sitemap({
+      hostname: HOSTNAME,
+    }),
+  ],
 });
