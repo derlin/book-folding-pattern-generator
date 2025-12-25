661759eafd62b3ef0b77528bf85387c704f22495
Lucy Linder <lucy.derlin@gmail.com>
2025-12-25 18:45:33 +0100

    feat: add canonical url

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import sitemap from 'vite-plugin-sitemap';
 
-const BASE_URL = process.env.APP_BASE_URL || '';
+const BASE_URL = process.env.VITE_BASE_URL || '';
 const HOSTNAME = BASE_URL || 'http://localhost:5173';
 
 export default defineConfig({
